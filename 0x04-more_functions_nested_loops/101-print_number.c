4432c062b17956bd8487a95fe28924fcb5831189
EMMANUEL CHARLES OYUGI <oyugi288@gmail.com>
2023-06-23 16:54:11 -0700

    more functions and more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,23 +4,24 @@
  * @n: int type number
  * Description: Can only use _putchar to print.
  */
-void print_number(int n) {
-    if (n == 0) {
-        _putchar('0');
-        return;
-    }
-
+void print_number(int n) 
+{
     if (n < 0) {
         _putchar('-');
         n = -n;
     }
 
+    if (n == 0) {
+        _putchar('0');
+        return;
+    }
+
     int divisor = 1;
-    while (n / divisor >= 10) {
+    while (n / divisor > 9) {
         divisor *= 10;
     }
 
-    while (divisor != 0) {
+    while (divisor > 0) {
         int digit = n / divisor;
         _putchar('0' + digit);
         n %= divisor;
