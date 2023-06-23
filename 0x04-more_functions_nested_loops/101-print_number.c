5938827c6cbf3119c6568dc24572e6ee7780a6f4
EMMANUEL CHARLES OYUGI <oyugi288@gmail.com>
2023-06-23 16:47:15 -0700

    more functions and more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,36 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - Function that prints an integer.
  * @n: int type number
  * Description: Can only use _putchar to print.
  */
-void print_number(int n)
-{
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
-
-	num = n;
-	/* negatives */
-	if (num < 0)
-	{
-		num *= -1;
-		_putchar('-');
-	}
-
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-
-	/* count down */
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+void _putchar(char c) {
+    // Implement the _putchar function according to your requirements
+    // This function should write a single character to the standard output
 }
+
+void print_number(int n) {
+    if (n == 0) {
+        _putchar('0');
+        return;
+    }
+
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    int divisor = 1;
+    while (n / divisor >= 10) {
+        divisor *= 10;
+    }
+
+    while (divisor != 0) {
+        int digit = n / divisor;
+        _putchar('0' + digit);
+        n %= divisor;
+        divisor /= 10;
+    }
+}
+
