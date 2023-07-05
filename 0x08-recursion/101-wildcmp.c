5574906c4960cd162679a2229d4207b445f89484
EMMANUEL CHARLES OYUGI <oyugi288@gmail.com>
2023-07-05 16:47:48 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,58 +1,42 @@
 #include "main.h"
-
 /**
- * wildcmp - compare two strings with "wildcard expansion" capabilities
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if strings can be considered identical, else 0
+ * wildcmp - Compare two strings allowing wildcard *
+ * @s1: First string
+ * @s2: Second string with wildcard *
+ *
+ * Return: 1 if the strings are considered identical, 0 otherwise
  */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0') /* If s2 ends with *, return 1 */
+			return (1);
+		else if (*s1 == '\0') /* If s1 ends but s2 has more characters after *, return 0 */
+			return (0);
+		else if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			/*
+			 * Recursively check if s1 matches the remaining part of s2
+			 * or if s1 is skipped and s2 continues with the next character after *
+			 */
+			return (1);
+		else
+			return (0);
+	}
 	else if (*s1 == '\0' || *s2 == '\0')
 	{
-		if (*s1 == '\0' && *s2 == '*')
-			return wildcmp(s1, ++s2);
-		else if (*s1 == '*' && *s2 == '\0')
-			return wildcmp(++s1, s2);
+		/* If either s1 or s2 ends, return 0 (not identical) */
 		return (0);
 	}
-
-	if (*s1 == *s2)
+	else if (*s1 != *s2)
 	{
-		return wildcmp(++s1, ++s2);
+		/* If characters at the current positions do not match, return 0 */
+		return (0);
 	}
-	else if (*s1 == '*')
+	else
 	{
-		if (*(s1 + 1) == '*')
-			return wildcmp(++s1, s2);
-		else
-		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
-		}
+		/* Move to the next characters and continue comparing */
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return wildcmp(s1, ++s2);
-		else
-		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
-		}
-	}
-
-	return (0);
-
 }
 
-int findsrc(char *s, char c, int i, int p)
-{
-	if (*(s + i) == '\0')
-		return (p + 1);
-	else if (*(s + i) == c || *(s + i) == '*')
-		p = i;
-
-	return (findsrc(s, c, i + 1, p));
-}
