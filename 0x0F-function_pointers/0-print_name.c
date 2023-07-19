035777b1736dace62ba29ac68ccb3ebccc6803b4
EMMANUEL CHARLES OYUGI <oyugi288@gmail.com>
2023-07-19 14:06:56 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,30 +1,14 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
-int _putchar(char c)
-{
-    /* Implementation of _putchar goes here */
-    return write(1, &c, 1);
-}
-
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: callback function that prints name
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
-}
+	if (!f)
+		return;
+	(*f)(name);
 
-void print_char(char *c)
-{
-    _putchar(*c);
-}
-
-void print_name_char_by_char(char *name)
-{
-    while (*name)
-    {
-        _putchar(*name);
-        name++;
-    }
 }
