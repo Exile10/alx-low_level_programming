d4dc18e7357cff585026ee16fb1e5cbe38f4db73
EMMANUEL CHARLES OYUGI <oyugi288@gmail.com>
2023-07-27 12:40:14 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
  *
- * Return: returns the address to the new element or NULL
- * if failed
-*/
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
 
+/**
+ * add_node - Add a new node at the beginning of a list_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @str: String to be duplicated and added to the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	list_t *new_node;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
+	/* Allocate memory for the new node */
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		free(new);
+		free(new_node);
 		return (NULL);
 	}
-	for (len = 0; str[len];)
-		len++;
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
+	/* Set the next pointer of the new node to the current head */
+	new_node->next = *head;
 
-	*head = new;
+	/* Update the head to point to the new node */
+	*head = new_node;
 
-	return (new);
-
+	return (new_node);
 }
 
