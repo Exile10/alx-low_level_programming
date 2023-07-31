62710339bcab474dc70a2f9c2545d51b08f546ab
EMMANUEL CHARLES OYUGI <oyugi288@gmail.com>
2023-07-31 09:00:17 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,39 +1,28 @@
-#include "lists.h"
-#include <stdio.h>
-/**
- * reverse_listint - Reverse a `listint_t` linked list
- * @head: double pointer to head
- * Description: Limited to going through loop only once.
- * Not allowed to use `malloc` or `free` or arrays
- * Can only declare a max of 2 variables.
- * Return: pointer to first node of reversed list
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *hold;
-	listint_t *current;
+#include <stdlib.h>
 
-	if (*head == NULL)
-		return (NULL);
+/* Definition for singly-linked list */
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-	current = *head;
-	*head = current->next;
-	hold = (*head)->next;
-	current->next = NULL;
-	if (*head == NULL)
-	{
-		*head = current;
-		return (current);
-	}
+/* Function to reverse a linked list */
+listint_t *reverse_listint(listint_t **head) {
+    if (*head == NULL || (*head)->next == NULL)
+        return *head;
 
-	while (hold != NULL)
-	{
-		(*head)->next = current;
-		current = *head;
-		*head = hold;
-		hold = (*head)->next;
-	}
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
 
-	(*head)->next = current;
-	return (*head);
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+    return *head;
 }
+
